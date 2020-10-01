1f4bd1da12749c113e583f39644d2ac0b70d87db
Max Klenk <max@klenk.biz>
2020-10-02 00:58:58 +0200

    add codespace Dockerfile

--- .devcontainer/install.sh
+++
@@ -1,6 +0,0 @@
-# Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Allow access to synapse folder
-chmod 777 synapse
--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ git clone --depth 1 https://github.com/hpi-schul-cloud/schulcloud-client.git
 git clone --depth 1 https://github.com/hpi-schul-cloud/schulcloud-server.git
 git clone --depth 1 https://github.com/hpi-schul-cloud/schulcloud-synapse-synchronization.git
 
+# Allow access to synapse folder
+chmod 777 synapse
+
 # Start containers
 docker-compose up -d
 
